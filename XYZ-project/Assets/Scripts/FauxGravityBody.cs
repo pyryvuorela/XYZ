6bdebf27f4f67de8865f7a5208b65dabc3d4fdbc
pyryvuorela <pyyrvu@utu.fi>
2015-06-15 10:22:56 +0300

    uudet Scriptit

---
+++ XYZ-project/Assets/Scripts/FauxGravityBody.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class FauxGravityBody : MonoBehaviour {
+
+	public FauxGravityAttractor attractor;
+	private Transform myTransform;
+
+	void Start () {
+		GetComponent<Rigidbody2D>().useGravity = false;
+		GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints.FreezeRotation;
+
+		myTransform = transform;
+	}
+
+	void FixedUpdate () {
+		if (attractor){
+			attractor.Attract(myTransform);
+		}
+	}
+	
+}
