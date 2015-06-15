22a4c6ed8d2b666300d536d457cca659cfa3303a
pyryvuorela <pyyrvu@utu.fi>
2015-06-15 10:38:35 +0300

    UUdet

--- XYZ-project/Assets/Scripts/FauxGravityBody.cs
+++ XYZ-project/Assets/Scripts/FauxGravityBody.cs
@@ -1,16 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof (Rigidbody))]
+[RequireComponent (typeof (Rigidbody2D))]
 public class FauxGravityBody : MonoBehaviour {
 
 	public FauxGravityAttractor attractor;
 	private Transform myTransform;
 
 	void Start () {
-		GetComponent<Rigidbody2D>().useGravity = false;
-		GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints.FreezeRotation;
-
 		myTransform = transform;
 	}
 
