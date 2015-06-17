add3b5a2399af940d915a8aac021af2914ac7fdc
pyryvuorela <pyyrvu@utu.fi>
2015-06-18 00:53:21 +0300

    Gravitaation säätö
    Pelaaja pystyy "loikkaamaan" planeetalta toiselle siirtyen sen
    gravitaatio kenttään

--- XYZ-project/Assets/Scripts/FauxGravityAttractor.cs
+++ XYZ-project/Assets/Scripts/FauxGravityAttractor.cs
@@ -4,18 +4,26 @@ using System.Collections;
 public class FauxGravityAttractor : MonoBehaviour {
 
 	public float gravity = -12;
-	public bool gravityField = false;
+	public float gravityField = 100;
+	public float dist = 0;
+	private Transform body = null;
 
-	public void Attract(Transform body) {
+	public void Update(){
+	GameObject other = GameObject.FindGameObjectWithTag ("Player");
+		body = other.transform;
+	
 		Vector3 gravityUp = (body.position - transform.position).normalized;
 		Vector3 localUp = body.up;
+		dist = Vector3.Distance (transform.position, body.position);
 
 
-
+		if (gravityField > Vector3.Distance(transform.position,body.position)) {
 			body.GetComponent<Rigidbody2D> ().AddForce (gravityUp * gravity);
 
 			Quaternion targetRotation = Quaternion.FromToRotation (localUp, gravityUp) * body.rotation;
 			body.rotation = Quaternion.Slerp (body.rotation, targetRotation, 50f * Time.deltaTime);
-				}
+		}
+
+	}
 
 }
