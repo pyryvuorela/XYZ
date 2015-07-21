6b136fee666657b7ba9e7fbb5281d49fc8d8e38e
pyryvuorela <pyyrvu@utu.fi>
2015-07-21 18:53:08 +0300

    Menun ja valikon luonti

---
+++ XYZ-project/Assets/Scripts/LevelLoadingScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoadingScript : MonoBehaviour {
+	
+
+	
+	public void LoadScene(int level)
+	{
+		Application.LoadLevel(level);
+	}
+}
