401520681e27b7206d7789b98cc4db2436061e2b
pyryvuorela <pyyrvu@utu.fi>
2016-02-18 11:29:54 +0200

    d

---
+++ August/Assets/Scripts/PlayMovie.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(AudioSource))]
+public class PlayMovie : MonoBehaviour {
+
+    public MovieTexture movie;
+    private AudioSource audio;
+
+	void Start () {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        audio = GetComponent<AudioSource>();
+        audio.clip = movie.audioClip;
+        movie.Play();
+        audio.Play();
+	}
+	
+
+	void Update () {
+	
+	}
+}
